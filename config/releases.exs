0c8fafb988f3bf41e236d94a56ef87f9f233ca8a
Chris Hildebrand <chris.hildebrand@versussystems.com>
2020-09-02 15:24:43 -0400

    Step 13

---
+++ config/releases.exs
@@ -0,0 +1,7 @@
+import Config
+
+config :nook_book, NookBookWeb.Endpoint,
+  server: true,
+  http: [port: 4000],
+  url: [host: "localhost"],
+  secret_keybase: "3IXO8jpmhBzRRxtOiCVBaCjsUAzQeg/3U83QUBiakA5NiiIxrUUr1F99YsPZkSzE"
